9d31ff3128023933d7321d1371bd5d839c3005a7
Hrishi Raj <hrishiraj1570@gmail.com>
2021-07-31 16:02:13 +0530

    modified

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 var text = `Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, a search engine, cloud computing, software, and hardware. It is considered one of the big four technology companies along with Amazon, Facebook, and Apple. Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14% of its publicly-listed shares and control 56% of the stockholder voting power through super-voting stock. The company went public via an initial public offering (IPO) in 2004. In 2015, Google was reorganized as a wholly owned`;
-// var correct = ;
-function replaceChar(origString, replaceChar, index) {
-    let newStringArray = origString.split("");
 
-    newStringArray[index] = replaceChar;
-
-    let newString = newStringArray.join("");
-
-    return newString;
-}
+const nonEditableTextArray = text.split("");
+var textArray = text.split("");
 document.getElementById('content-text').innerHTML = text;
 
 function click(event) {
     var typedChar = event.data;
-    let contentChar = text.charAt(document.getElementById('typed-text').value.length - 1);
-    console.log(typedChar, contentChar, typedChar == contentChar);
-    if (typedChar == contentChar) {
-        // HTMLTextAreaElement.style.backgroundColor = 'red';
-        let r = `<span class="correct">${typedChar}</span>`
-        replaceChar(text, r, (document.getElementById('typed-text').value.length - 1));
+    console.log(event);
+    let charIndex = document.getElementById('typed-text').value.length - 1;
+    let contentChar = nonEditableTextArray[charIndex];
+    let r;
+    if (event.inputType == 'insertText') {
+        if (typedChar == contentChar) {
+            r = `<span class="correct">${nonEditableTextArray[charIndex]}</span>`;
+            textArray[charIndex] = r;
+            text = textArray.join("");
+            document.getElementById('content-text').innerHTML = text;
+            console.log("correct", charIndex, typedChar, contentChar);
+        } else if (typedChar != contentChar) {
+            r = `<span class="wrong">${nonEditableTextArray[charIndex]}</span>`;
+            textArray[charIndex] = r;
+            text = textArray.join("");
+            document.getElementById('content-text').innerHTML = text;
+            console.log("wrong", charIndex, typedChar, contentChar);
+        }
+    } else if (event.inputType == 'deleteContentBackward') {
+        r = nonEditableTextArray[charIndex + 1];
+        textArray[charIndex + 1] = r;
+        text = textArray.join("");
         document.getElementById('content-text').innerHTML = text;
-
+        console.log("wrong", charIndex, typedChar, contentChar);
     }
 };
 
 document.getElementById('typed-text').addEventListener('input', click);
+
+// Stoping selecting to input
+var inp = document.getElementById('typed-text');
+
+inp.addEventListener('select', function() {
+    this.selectionStart = this.selectionEnd;
+}, false);
